c3d02dda01de018e259526f1ebdbbb78770bd2a9
Bexook <andrik.mykytyn@gmail.com>
2022-02-27 12:29:01 +0200

    oih

--- src/main/java/com/example/ddoser/dto/Data.java
+++ src/main/java/com/example/ddoser/dto/Data.java
@@ -1,11 +1,14 @@
 package com.example.ddoser.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import java.util.List;
 
 @lombok.Data
 @Accessors(chain = true)
+@AllArgsConstructor
 public class Data {
 
     private List<String> emails;
